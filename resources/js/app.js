d261759e19e777d274749f131936723a5a5090ab
Noel Xavier Ulpindo <noel.ulpindo@gmail.com>
2023-01-24 08:19:48 +0800

    added vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,18 @@ import { createInertiaApp, Link,  Head } from "@inertiajs/inertia-vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+// vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
   resolve: (name) =>
@@ -14,9 +26,10 @@ createInertiaApp({
       import.meta.glob("./pages/**/*.vue")
     ),
   setup({ el, app, props, plugin }) {
-    return createApp({ render: () => h(app, props) })
+    return createApp({  render: () => h(app, props) })
       .use(plugin)
       .use(ZiggyVue, Ziggy)
+      .use(vuetify)
       .component('Link', Link)
       .component('Head', Head)
       .mount(el);
--- resources/js/pages/app/index.vue
+++ resources/js/pages/app/index.vue
@@ -1,4 +1,9 @@
 <template>
+  <v-app>
+    <v-container>
+        <v-btn>Click Me!</v-btn>
+    </v-container>
+  </v-app>
   <p>Hello from index</p>
   <Link :href="route('app.test')"> LINK TO TEST </Link>
 </template>
